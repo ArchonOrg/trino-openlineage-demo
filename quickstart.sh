804c95a0f3be29aca1affa31e7890e260b541060
sj-ganwh <155623319+sj-ganwh@users.noreply.github.com>
2024-11-12 02:59:58 +0000

    add quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -1,28 +1,18 @@
-# openlineage-demo
+#!/usr/bin/env bash
 
-## Quickstart
+set -e
 
-### All-in-one script
-
-```sh
-bash quickstart.sh
-```
-
-## (Optional) Setup Details
-
-### Service Up
-
-```sh
 # openlineage
 (git clone --branch 0.50.0 --depth 1 https://github.com/MarquezProject/marquez && cd marquez && ./docker/up.sh -d)
 
 # trino
 docker run -d --name trino -v $(pwd)/trino:/etc/trino --network host trinodb/trino:464
-```
 
-### SQL demo
+# check if trino is up
+while [ "$(docker ps | grep trino | grep healthy | wc -l)" -eq 0 ] ; do echo "Waiting for trino, sleep 3 secs"; sleep 3; done 
 
-```sql
+# run sql
+docker exec -it trino trino --execute "
 create table memory."default".cust_order as
 select
     t1.orderkey, t1.orderdate, coalesce(t1.totalprice, 1) as totalprice,
@@ -35,14 +25,12 @@ from tpch.sf1.orders t1
 join tpch.sf1.customer t2
     on t1.custkey = t2.custkey
 limit 10;
-```
+"
 
-### openlineage webui
-
-
-```
+# openlineage webui
+cat <<\EOF
 Browse: 
 - http://localhost:3000/
 Or more specifically (if the URL isn't changed): 
 - http://localhost:3000/lineage/dataset/trino%3A%2F%2Flocalhost%3A8080/memory.default.cust_order
-```
+EOF
